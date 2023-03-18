800d26fbf295eb6e7efa7b0084593c4028f8569f
runovrv <runovrv@spb.psbank.ru>
2023-03-18 11:30:53 +0300

    Initial Commit

---
+++ weatherForecast/weatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherForecastApp.swift
+//  weatherForecast
+//
+//  Created by Роман on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
